2c867662baecfd3fb7bf7c490e1303069fe5bd14
Suhrob <suhrobgithub22@gmail.com>
2024-10-12 10:07:35 +0500

    style update

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,34 +1,38 @@
 import logo from './sariqdasturchi.png';
 import './header.css'
 import { Link } from 'react-router-dom';
+import { CiMenuFries } from "react-icons/ci";
 
 function HeaderComponent() {
-    return(
+    return (
         <>
-         <header>
-            <nav>
-                <div className="logo">
-                    <a href="/">
-                        <span><img src={logo} alt="Sariqdastuchi-logo" /></span>
-                        <h1>Sariqdasturchi</h1>
-                    </a>
-                </div>
-                <div className="menu-list">
-                    <ul>
-                        <li><Link to={'/'}>Bosh sahifa</Link></li>
-                        <li><Link to={'/course'}>Barcha kurslar</Link></li>
-                        <li><Link to={'/'}>Loyihalar</Link></li>
-                        <li><Link to={'/contact-me'}>Bog'lanish</Link></li>
-                    </ul>
-                </div>
-                <div className="login-page">
-                    <Link to={'/login'}>
-                        <button>Kirish</button>
-                    </Link>
-                </div>
-                <div className="user-page"></div>
-            </nav>
-         </header>
+            <header>
+                <nav>
+                    <div className="logo">
+                        <a href="/">
+                            <span><img src={logo} alt="Sariqdastuchi-logo" /></span>
+                            <h1>Sariqdasturchi</h1>
+                        </a>
+                    </div>
+                    <div className="menu-list">
+                        <ul>
+                            <li><Link to={'/'}>Bosh sahifa</Link></li>
+                            <li><Link to={'/course'}>Barcha kurslar</Link></li>
+                            <li><Link to={'/'}>Loyihalar</Link></li>
+                            <li><Link to={'/contact-me'}>Bog'lanish</Link></li>
+                        </ul>
+                    </div>
+                    <div className="login-page">
+                        <Link to={'/login'}>
+                            <button>Kirish</button>
+                        </Link>
+                    </div>
+                    <div className="menu">
+                        <button><CiMenuFries /></button>
+                    </div>
+                    <div className="user-page"></div>
+                </nav>
+            </header>
         </>
     )
 }
